a43ae1f0973d64610ad4a65b275e53e3e53fb5aa
Agnish Chakraborty <30952790+agnishcc@users.noreply.github.com>
2024-04-02 17:34:54 +0530

    feat(backend): 4 adds packages for api keys
    adds two functions to obfuscate and generate api keys
    
    Closes: 4

---
+++ packages/apikey/index.ts
@@ -0,0 +1,48 @@
+/**
+ * @description Generates a random API key
+ * 
+ * @returns a apiKey string
+ * @type {Function}
+ * @example
+ * const apiKey = generateApiKey();
+ * console.log(apiKey);
+ * // Output: 3d4f5g6h7j8k9l0
+ * 
+ */
+
+export const generateApiKey = (): string => {
+    return Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+}
+
+
+/**
+ * @description Obfuscates an API key
+ * 
+ * @param {string} apiKey - The API key to obfuscate
+ * 
+ * @param {Object} visibility - The visibility of the API key
+ * @param {number} visibility.start - The number of visible characters at the start of the API key. Default is 3
+ * @param {number} visibility.end - The number of visible characters at the end of the API key. Default is 4
+ * 
+ * @returns a obfuscated API key string
+ * @type {Function}
+ * @example
+ * const apiKey = '3d4f5g6h7j8k9l0';
+ * const obfuscatedApiKey = obfuscateApiKey(apiKey);
+ * console.log(obfuscatedApiKey);
+ * // Output: 3d4***************k9l0
+* @example
+* const apiKey = 3d4f5g6h7j8k9l0';
+* const obfuscatedApiKey = obfuscateApiKey(apiKey, { start: 2, end: 2 });
+* console.log(obfuscatedApiKey);
+* // Output: 3d**********l0
+* 
+*/
+export const obfuscateApiKey = (apiKey: string, visibility?: {
+    start: number,
+    end: number
+}): string => {
+    const visibleStart = visibility?.start || 3;
+    const visibleEnd = visibility?.end || 4;
+    return apiKey.substring(0, visibleStart) + '*'.repeat(apiKey.length - (visibleStart - visibleEnd)) + apiKey.substring(apiKey.length - visibleEnd);
+}
